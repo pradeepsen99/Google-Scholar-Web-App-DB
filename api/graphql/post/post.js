87e074d6cca92ebe5fba6383575bb5e95edfc762
Pradeep Kumar <pradeepkumar@Pradeeps-MacBook-Pro.local>
2020-04-10 02:52:34 -0500

    Updated all commands for google scholar database

--- api/graphql/post/post.js
+++ api/graphql/post/post.js
@@ -2,7 +2,7 @@ const graphql = require("graphql");
 const sqlite3 = require('sqlite3').verbose();
 
 //create a database if no exists
-const database = new sqlite3.Database("../cs411project");
+const database = new sqlite3.Database("../cs411project.db");
 
 const createPostTable = () => {
     const  query  =  `
@@ -22,7 +22,7 @@ const createPostTable = () => {
 }
 
 //init table - commented out for now
-//createPostTable();
+createPostTable();
 
 
 
@@ -43,6 +43,24 @@ const PostType = new graphql.GraphQLObjectType({
     }
 });
 
+
+/* 
+query{
+  Articles
+  {
+    article_id,
+    title, 
+    citedBy, 
+    citations, 
+    pub_year, 
+    eprint, 
+    pub_number, 
+    pub_publisher, 
+    pub_url, 
+    journal
+  }
+}
+*/
 
 // create a graphql query to select all and by id
 var queryType = new graphql.GraphQLObjectType({
@@ -67,11 +85,11 @@ var queryType = new graphql.GraphQLObjectType({
         Article:{
             type: PostType,
             args:{
-                article_id:{
+                id:{
                     type: new graphql.GraphQLNonNull(graphql.GraphQLID)
                 }               
             },
-            resolve: (root, {article_id}, context, info) => {
+            resolve: (root, {id}, context, info) => {
                 return new Promise((resolve, reject) => {
                 
                     database.all("SELECT * FROM Article WHERE article_id = (?);",[article_id], function(err, rows) {                           
@@ -85,113 +103,192 @@ var queryType = new graphql.GraphQLObjectType({
         }
     }
 });
+
 /*
+mutation {
+  createArticle(
+    title: "test title", 
+    citedBy: "test citedby", 
+    citations:69, 
+    pub_year:1969, 
+    eprint:"test eprint", 
+    pub_number:420, 
+    pub_publisher:"test pub_publisher", 
+    pub_url:"test pub_url", 
+    journal:"test journal")
+  {
+    title, 
+    citedBy, 
+    citations, 
+    pub_year, 
+    eprint, 
+    pub_number, 
+    pub_publisher, 
+    pub_url, 
+    journal
+  }
+}
+*/
+
 //mutation type is a type of object to modify data (INSERT,DELETE,UPDATE)
 var mutationType = new graphql.GraphQLObjectType({
     name: 'Mutation',
     fields: {
       //mutation for creacte
-      createPost: {
+      createArticle: {
         //type of object to return after create in SQLite
         type: PostType,
         //argument of mutation creactePost to get from request
         args: {
-          title: {
-            type: new graphql.GraphQLNonNull(graphql.GraphQLString)
-          },
-          description:{
-              type: new graphql.GraphQLNonNull(graphql.GraphQLString)
-          },
-          createDate:{
-              type: new graphql.GraphQLNonNull(graphql.GraphQLString)
-          },
-          author:{
-              type: new graphql.GraphQLNonNull(graphql.GraphQLString)
-          }
+            title: {
+                type: new graphql.GraphQLNonNull(graphql.GraphQLString)
+            },
+            citedBy:{
+                type: new graphql.GraphQLNonNull(graphql.GraphQLString)
+            },
+            citations:{
+                type: new graphql.GraphQLNonNull(graphql.GraphQLID)
+            },
+            pub_year:{
+                type: new graphql.GraphQLNonNull(graphql.GraphQLID)
+            },
+            eprint:{
+                type: new graphql.GraphQLNonNull(graphql.GraphQLString)
+            },
+            pub_number:{
+                type: new graphql.GraphQLNonNull(graphql.GraphQLID)
+            },
+            pub_publisher:{
+                type: new graphql.GraphQLNonNull(graphql.GraphQLString)
+            },
+            pub_url:{
+                type: new graphql.GraphQLNonNull(graphql.GraphQLString)
+            },
+            journal:{
+                type: new graphql.GraphQLNonNull(graphql.GraphQLString)
+            }
         },
-        resolve: (root, {title, description, createDate, author}) => {
+        resolve: (root, {title, citedBy, citations, pub_year, eprint, pub_number, pub_publisher, pub_url, journal}) => {
             return new Promise((resolve, reject) => {
                 //raw SQLite to insert a new post in post table
-                database.run('INSERT INTO Posts (title, description, createDate, author) VALUES (?,?,?,?);', [title, description, createDate, author], (err) => {
+                database.run('INSERT INTO Article (title, citedBy, citations, pub_year, eprint, pub_number, pub_publisher, pub_url, journal) VALUES (?,?,?,?,?,?,?,?,?);', [title, citedBy, citations, pub_year, eprint, pub_number, pub_publisher, pub_url, journal], (err) => {
                     if(err) {
                         reject(null);
                     }
-                    database.get("SELECT last_insert_rowid() as id", (err, row) => {
-                        
+                    database.get("SELECT last_insert_rowid() as article_id", (err, row) => {
+                        //TODO if unnecessary stuff prints delete
                         resolve({
-                            id: row["id"],
-                            title: title,
-                            description: description,
-                            createDate:createDate,
-                            author: author
+                            article_id: row["article_id"],
+                            title: title, 
+                            citedBy: citedBy, 
+                            citations: citations, 
+                            pub_year: pub_year, 
+                            eprint: eprint, 
+                            pub_number: pub_number, 
+                            pub_publisher: pub_publisher, 
+                            pub_url: pub_url, 
+                            journal: journal
                         });
                     });
                 });
             })
         }
       },
+
+      /*
+        mutation {
+            updateArticle(
+                article_id: 10001,
+                title: "updated title", 
+                citedBy: "",
+                citations: 100, 
+                pub_year:2020, 
+                eprint:"updated eprint", 
+                pub_number:2019, 
+                pub_publisher:"updated pub_publisher", 
+                pub_url:"updated pub_url", 
+                journal:"updated journal"
+            )
+        }
+      */
       //mutation for update
-      updatePost: {
+      updateArticle: {
         //type of object to return afater update in SQLite
         type: graphql.GraphQLString,
         //argument of mutation creactePost to get from request
         args:{
-            id:{
+            article_id:{
                 type: new graphql.GraphQLNonNull(graphql.GraphQLID)
             },
             title: {
                 type: new graphql.GraphQLNonNull(graphql.GraphQLString)
             },
-            description:{
-                  type: new graphql.GraphQLNonNull(graphql.GraphQLString)
+            citedBy:{
+                type: new graphql.GraphQLNonNull(graphql.GraphQLString)
             },
-            createDate:{
-                  type: new graphql.GraphQLNonNull(graphql.GraphQLString)
+            citations:{
+                type: new graphql.GraphQLNonNull(graphql.GraphQLID)
             },
-            author:{
-                  type: new graphql.GraphQLNonNull(graphql.GraphQLString)
-            }             
+            pub_year:{
+                type: new graphql.GraphQLNonNull(graphql.GraphQLID)
+            },
+            eprint:{
+                type: new graphql.GraphQLNonNull(graphql.GraphQLString)
+            },
+            pub_number:{
+                type: new graphql.GraphQLNonNull(graphql.GraphQLID)
+            },
+            pub_publisher:{
+                type: new graphql.GraphQLNonNull(graphql.GraphQLString)
+            },
+            pub_url:{
+                type: new graphql.GraphQLNonNull(graphql.GraphQLString)
+            },
+            journal:{
+                type: new graphql.GraphQLNonNull(graphql.GraphQLString)
+            }         
         },
-        resolve: (root, {id, title, description, createDate, author}) => {
+        resolve: (root, {article_id, title, citedBy, citations, pub_year, eprint, pub_number, pub_publisher, pub_url, journal}) => {
             return new Promise((resolve, reject) => {
                 //raw SQLite to update a post in post table
-                database.run('UPDATE Posts SET title = (?), description = (?), createDate = (?), author = (?) WHERE id = (?);', [title, description, createDate, author, id], (err) => {
+                database.run('UPDATE Article SET title = (?), citedBy = (?), citations = (?), pub_year = (?),eprint = (?), pub_number = (?), pub_publisher = (?), pub_url = (?), journal = (?) WHERE article_id = (?);', [title, citedBy, citations, pub_year, eprint, pub_number, pub_publisher, pub_url, journal, article_id], (err) => {
                     if(err) {
                         reject(err);
                     }
-                    resolve(`Post #${id} updated`);
+                    //TODO if unnecessary stuff prints delete
+                    resolve(`Post #${article_id} updated`);
                 });
             })
         }
       },
-      //mutation for update
-      deletePost: {
+      //mutation for delete
+      deleteArticle: {
          //type of object resturn after delete in SQLite
         type: graphql.GraphQLString,
         args:{
-            id:{
+            article_id:{
                 type: new graphql.GraphQLNonNull(graphql.GraphQLID)
             }               
         },
-        resolve: (root, {id}) => {
+        resolve: (root, {article_id}) => {
             return new Promise((resolve, reject) => {
-                //raw query to delete from post table by id
-                database.run('DELETE from Posts WHERE id =(?);', [id], (err) => {
+                //raw query to delete from post table by article_id
+                database.run('DELETE from Article WHERE article_id =(?);', [article_id], (err) => {
                     if(err) {
                         reject(err);
                     }
-                    resolve(`Post #${id} deleted`);                    
+                    resolve(`Post #${article_id} deleted`);                    
                 });
             })
         }
       }
     }
-});*/
+});
 
 //define schema with post object, queries, and mustation 
 const schema = new graphql.GraphQLSchema({
     query: queryType,
-    //mutation: mutationType 
+    mutation: mutationType 
 });
 
 //export schema to use on index.js
