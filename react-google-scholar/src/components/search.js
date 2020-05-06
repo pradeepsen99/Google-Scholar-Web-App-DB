07f3103ce1b71527b40adb2ffc9c43b93e581c2a
zjuiuczy <zhaoyuc2@illinois.edu>
2020-05-06 11:10:01 -0500

    search function implementation

--- react-google-scholar/src/components/search.js
+++ react-google-scholar/src/components/search.js
@@ -3,9 +3,11 @@ import gql from 'graphql-tag';
 import { graphql} from "react-apollo";
 import * as compose from 'lodash.flowright';
 import debounce from 'debounce';
+import {useLazyQuery} from '@apollo/react-hooks';
+import ArticleItem from "./ArticleItem";
 const searchArticle = gql `
-    query searchArticle{
-      searchArticle(search_query:"is"){
+    query searchArticle($search_query:String!){
+      searchArticle(search_query:$search_query){
         article_id,
         title,
         citedBy,
@@ -19,105 +21,39 @@ const searchArticle = gql `
       }
     }
 `;
-const Paging = gql`
-query {
-  Paging (offset:0, limit:1000){
-    article_id,
-    title,
-    citedBy,
-    citations,
-    pub_year,
-    eprint,
-    pub_number,
-    pub_publisher,
-    pub_url,
-    journal
-  }
-}
-`;
-class Search extends Component{
-   state = {
-     search_query: ''
-   }
-   onChange = (e) => {
-     const value = e.target.value
-     this.handleFilter(value)
-   }
-   handleFilter = debounce((value) => {
-     this.props.onSearch(value);
-   },250)
-   render(){
-     const { loading } = this.props.data;
-     console.log(this.props.data.searchArticle);
-     const items = this.props.data.searchArticle;
-     return (
-       <div>
-        <input
-          style={styles.input}
-          onChange={this.onChange.bind(this)}
-          placeholder='Search for articles' />
-          {
-            !!loading&&(<p>Searching...</p>)
-          }
-          {
-            !loading&&!items.length&&(<p>Sorry, no results.</p>)
-          }
-          {
-            !loading&&this.props.data.searchArticle.map(searchArticle => (
-              <div key={searchArticle.title} style={styles.container}>
-              <p style={styles.title}>{searchArticle.title}</p>
-              <p style={styles.description}>{searchArticle.article_id}</p>
-              </div>
-            ))
-          }
-          </div>
-     )
-   }
-  }
-  
-  export default compose(
-    graphql(searchArticle, {
-      options: data => ({
-        fetchPolicy: 'cache-and-network'
-      }),
-      props: props => ({
-        onSearch: search_query => {
-          search_query = search_query.toLowerCase()
-          return props.data.fetchMore({
-            query: search_query === '' ? Paging:searchArticle,
-            variables: {
-              search_query
-            },
-            updateQuery: (previousResult, { fetchMoreResult }) => ({
-              ...previousResult,
-              Paging: {
-                ...previousResult.Paging,
-                items: fetchMoreResult.Paging.items
-              }
-            })
-          })
-        },
-        data: props.data
-      })
-    })
-  )(Search);
-  const styles = {
-    container: {
-      padding: 10,
-      borderBottom: '1px solid #ddd'
-    },
-    title: {
-      fontSize: 18
-    },
-    description: {
-      fontSize: 15,
-      color: 'rgba(0, 0, 0, .5)'
-    },
-    input: {
-      height: 40,
-      width: 300,
-      padding: 7,
-      fontSize: 15,
-      outline: 'none'
+const Search = () => {
+  const [runQuery, {data, loading, error}] = useLazyQuery(searchArticle);
+  if(data){
+    console.log(data.searchArticle);
+    return (
+      <Fragment>{
+      data.searchArticle.map(searchArticle => (
+        <ArticleItem key={searchArticle.article_id} Paging={searchArticle} />
+      ))                            
     }
+    </Fragment> )
   }
+  let search_query = '';
+  return (
+    <div id="edit-user">
+     <div className="field">
+        <label htmlFor="name">Search Title</label>
+        <input
+          type="text"
+          id="search_query"
+          onChange={(event) => {search_query = event.target.value }} />
+        <button onClick={
+          (event) => {
+            event.preventDefault();
+            runQuery({
+              variables: { search_query }, 
+              suspend: false
+            })
+          }}
+          className = "btn btn-success"
+          >Search</button>
+        </div>
+      </div>
+  );
+}
+export default Search;
